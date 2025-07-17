f1367ddc3bc0e99dc5746fe20f8af3783299dd19
Iamdachi04 <dachi4github@gmail.com>
2025-07-17 23:10:11 +0400

    okay , so here I added the functionality to the button that I created for actually translating (well not ACTUALLY translating yet , it is simply just to get this text to the backend (on port 8080) , and then get the response back , and then we write it back to the text format on to the App.tsx)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,8 +18,30 @@ function App() {
   const [inputText, setInputText] = useState<string>('');
   const [translatedText, setTranslatedText] = useState<string>('');
   const handleTranslate = async () => {
-   
+    //აქ შემოგვყავს ტექსტი , რომელიც უნდა ითარგმნოს
+    const requestBody: TranslationRequest = { text: inputText };
+    //აქ ვაკეთებთ თხოვნას სერვერზე, რომ ტექსტი ითარგმნოს , და პასუხსაც აქ ვინახავთ , ამ რესპონსში
+    const response = await fetch('http://localhost:8080/api/translate', {
+      //აქ ვაყენებთ მეთოდს პოსტი, რადგან ჩვენ ვგზავნით მონაცემებს სერვერზე და
+      // ვაყენებთ ჰედერებს, რომ სერვერმა იცოდეს, რომ ჩვენ ჯეისონის მონაცემებს ვგზავნით
+      method: 'POST',
+      
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      //აქ ვგზავნით მონაცემებს სერვერზე, რომ ტექსტი ითარგმნოს  , ანუ თითონ ტექსტს ვწერთ აქ.
+      body: JSON.stringify(requestBody),
+    });
+    if (!response.ok) {
+      const errorData: ErrorResponse = await response.json();
+      throw new Error(errorData.message || `HTTP error! status: ${response.status}`);
+    }
+    // თუ ყველაფერი კარგადაა, ვიღებთ პასუხს    
+    const data: TranslationResponse = await response.json();
+    // და ვაყენებთ ამ პასუხს სტეიტში, რომ ჩვენ შეგვიძლია გამოვიყენოთ ეს ტექსტი
+    setTranslatedText(data.translatedText);
   };
+
 
   return (
     <div className="App">
